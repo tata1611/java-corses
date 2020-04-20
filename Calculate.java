a3c0708b9d1ee67ab66b4eaf2f25991af142bd1e
Tata1611 <ooorubiconch@gmail.com>
2020-04-20 23:57:59 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,13 @@
+/** класс для вычисления суммы двух чисел. числа берутся из входного массива./
+*/
 public class Calculate{
 	public static void main(String[]arg){
 		System.out.println("Calculate...");
+		
+		int first1=Integer.valueOf(arg[0]);
+		int second1=Integer.valueOf(arg[1]);
+		int minus=first1/second1;
+		System.out.println("min  " : minus);
+		
 	}
 }
